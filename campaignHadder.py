28f41d9ca439d274ee38905d06ed1e7af13a0b8c
Eric Schanet <eric.schanet@cern.ch>
2019-05-28 15:59:47 +0200

    Script for hadding across campaigns

---
+++ campaignHadder.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import ROOT
+
+import os,sys,glob
+import argparse
+import re
+import pprint
+
+from machete import pythonHelpers as ph
+
+from slurmy import JobHandler, Slurm
+
+jh = JobHandler(work_dir="/project/etp3/eschanet/collect", name="campaignHadder", run_max=20)
+
+merge_script = """
+echo "running on $(hostname)"
+echo ""
+
+source /etc/profile.d/modules.sh
+module load root
+echo "using $(which root)"
+
+hadd -f {outputfile} {inputfiles}
+
+echo "removing files that have been merged to save diskspace"
+
+#rm  {inputfiles}
+"""
+
+parser = argparse.ArgumentParser(description='Hadd files across MC campaigns')
+parser.add_argument('input', help='Text file with samples to hadd.', nargs='+')
+parser.add_argument('-naming-scheme', help='Naming scheme of signal samples. Use regex.', default='C1N2_Wh_hbb_\d*p\d_\d*p\d')
+# parser.add_argument('-naming-scheme', help='Naming scheme of signal samples. Use regex.', default='[A-Za-z]{2}_onestepCC_\d*_\d*_\d*')
+parser.add_argument('-production', help='Production tag.', default='v2-0-6')
+# parser.add_argument('-output-file', help='Name of hadded output tree.', default='allTrees_v1_20_signal_wh.root')
+#parser.add_argument('-output-file', help='Name of hadded output tree.', default='allTrees_v1_20_signal_onestep.root')
+parser.add_argument('--skip-hadd', help='Skip the actual hadding. Useful for testing purposes', action='store_true')
+args = parser.parse_args()
+
+pattern = re.compile(args.naming_scheme)
+
+base_path = "/project/etp2/eschanet/OneLepAnalysis-rel21-latest/SusySkim1LInclusive_submodules/source/SusySkim1LInclusive/data/samples/mc16e/"
+full_paths = [os.path.join(base_path, path) for path in args.input]
+
+output_path = "/project/etp2/eschanet/trees/v2-0/merged/signal/"
+
+for f in full_paths:
+    with open(f,'r') as i:
+        for line in i:
+            if ph.is_comment(line):
+                continue
+            # print line
+            match = re.search(pattern,line)
+            if match:
+                point = match.group()
+
+                print point
+                #TODO: make nicer
+                if "onestep" in point.lower():
+                    #onestep points have different formatting at tree- and AOD-level
+                    point = point.replace("onestepCC", "oneStep")
+                seacher = "{}*merged_processed*.root".format(point)
+                files_mc16a = glob.glob("/project/etp4/eschanet/ntuples/common/mc16a/{}/merged/{}".format(args.production,seacher))
+                files_mc16d = glob.glob("/project/etp4/eschanet/ntuples/common/mc16d/{}/merged/{}".format(args.production,seacher))
+                files_mc16e = glob.glob("/project/etp4/eschanet/ntuples/common/mc16e/{}/merged/{}".format(args.production,seacher))
+
+                if len(files_mc16a)==0 or len(files_mc16d)==0 or len(files_mc16e)==0:
+                    print "mc16a: %i"%len(files_mc16a)
+                    print "mc16d: %i"%len(files_mc16d)
+                    print "mc16e: %i"%len(files_mc16e)
+                    print "WARNING  -  {} is not complete".format(point)
+
+                else:
+                    inputfiles = " ".join(sorted(files_mc16a)) + " " + " ".join(sorted(files_mc16d)) + " " + " ".join(sorted(files_mc16e))
+                    #pprint.pprint(inputfiles)
+                    outputfile = output_path + "{}_merged_processed.root".format(point)
+                    jh.add_job(name=point+"_campaignHadder", run_script=merge_script.format(outputfile=outputfile, inputfiles=inputfiles))
+
+jh.run_jobs()
